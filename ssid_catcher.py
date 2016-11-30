47dc8b490320b9c319e6c83d22494963529eba64
Florian Loch <florian.loch@gmail.com>
2016-11-30 09:31:51 +0100

    Add mac filter to ssid_catcher

--- ssid_catcher.py
+++ ssid_catcher.py
@@ -2,12 +2,15 @@
 
 import sys
 from analyzr_core import *
+import re
 
 
 class SSIDCatcher(IPacketAnalyzer):
 
-    def __init__(self):
+    def __init__(self, mac_filter):
         print "Running SSIDCatcher"
+
+        self._mac_filter = mac_filter
 
     def get_display_filter(self):
         return "wlan.fc.type_subtype == 4"
@@ -24,11 +27,20 @@ class SSIDCatcher(IPacketAnalyzer):
         if ssid == "SSID: ":
             return
 
+        if self._mac_filter != None and re.match(self._mac_filter, str(wlan.sa)) == None:
+            return
+
         print wlan.sa + " -> " + ssid
 
     def on_end(self):
         pass
 
-ssid_catcher = SSIDCatcher()
-core = AnalyzrCore(ssid_catcher)
+
+core = AnalyzrCore()
+
+core.get_arg_parser().add_argument("--mac", dest="mac_filter", default=None,
+                                      help="Filter by mac address of sender")
+
+ssid_catcher = SSIDCatcher(core.get_parsed_cli_options().mac_filter)
+core.register_handler(ssid_catcher)
 core.start()
